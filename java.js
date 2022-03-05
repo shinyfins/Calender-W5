9d89901c1215c0269719351e14650ce159409155
Your Name <mcpgco@gmail.com>
2022-03-05 13:44:37 -0700

    added time blocks to container div, not sure why its squished when i load page but well get around that soon enough

---
+++ java.js
@@ -0,0 +1,4 @@
+$(document).ready(function () {// essentially tells engine to load 1)html & 2)css first.
+    //display current day & time.
+    $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+});
