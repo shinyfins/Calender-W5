d43a5be7c1bad23f898f8d92ee2a0a7881f4055f
Your Name <mcpgco@gmail.com>
2022-03-07 23:16:09 -0700

    tested and ran all blocks of time and they save

--- java.js
+++ java.js
@@ -12,6 +12,13 @@ $(".saveBtn").on("click", function () {
 })
 
     $("#NineAm .description").val(localStorage.getItem("NineAm"));
-
+    $("#TenAm .description").val(localStorage.getItem("TenAm"));
+    $("#ElevenAm .description").val(localStorage.getItem("ElevenAm"));
+    $("#TwelveAm .description").val(localStorage.getItem("TwelveAm"));
+    $("#OnePm .description").val(localStorage.getItem("OnePm"));
+    $("TwoPm .description").val(localStorage.getItem("TwoPm"));
+    $("#ThreePm .description").val(localStorage.getItem("ThreePm"));
+    $("#FourPm .description").val(localStorage.getItem("FourPm"));
+    $("#FivePm .description").val(localStorage.getItem("FivePm"));
 
 });
