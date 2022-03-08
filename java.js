20df24462098fddac063f3cffb154803c3ef36c0
Your Name <mcpgco@gmail.com>
2022-03-07 23:20:49 -0700

    forgot hastah in hour 2

--- java.js
+++ java.js
@@ -12,13 +12,21 @@ $(".saveBtn").on("click", function () {
 })
 
     $("#NineAm .description").val(localStorage.getItem("NineAm"));
+
     $("#TenAm .description").val(localStorage.getItem("TenAm"));
+
     $("#ElevenAm .description").val(localStorage.getItem("ElevenAm"));
+
     $("#TwelveAm .description").val(localStorage.getItem("TwelveAm"));
+
     $("#OnePm .description").val(localStorage.getItem("OnePm"));
-    $("TwoPm .description").val(localStorage.getItem("TwoPm"));
+
+    $("#TwoPm .description").val(localStorage.getItem("TwoPm"));
+
     $("#ThreePm .description").val(localStorage.getItem("ThreePm"));
+
     $("#FourPm .description").val(localStorage.getItem("FourPm"));
+
     $("#FivePm .description").val(localStorage.getItem("FivePm"));
 
 });
