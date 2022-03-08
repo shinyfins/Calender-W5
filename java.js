9271f808402ced569b261ded5b6445ce739d5048
Your Name <mcpgco@gmail.com>
2022-03-07 22:57:27 -0700

    added current time stamp, couldnt get moment to communicate with the site so i just closed off the link and started with my own new link. after that it all started to work git add .git add .

--- java.js
+++ java.js
@@ -1,8 +1,5 @@
 $(document).ready(function () {// essentially tells engine to load 1)html & 2)css first.
     //display current day & time.
-    $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+  
 });
 
-
-
-$("#hour9 .description").val(localStorage.getItem("hour9"));
