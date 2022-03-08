3bb116c84fdcd681dbeeec88d747c0d929290789
Your Name <mcpgco@gmail.com>
2022-03-07 23:12:55 -0700

    java wasnt working well its cause i didnt properly link the java to thr html, im gonna assume you guys read the commits so im sorry for the late turn in but hey im getting it done git add .git add .

--- java.js
+++ java.js
@@ -1,5 +1,17 @@
 $(document).ready(function () {// essentially tells engine to load 1)html & 2)css first.
     //display current day & time.
-  
-});
 
+
+$(".saveBtn").on("click", function () {
+
+    console.log(this);
+    var text = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+
+    localStorage.setItem(time, text);
+})
+
+    $("#NineAm .description").val(localStorage.getItem("NineAm"));
+
+
+});
