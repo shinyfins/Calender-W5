7d9a2a9d0562bef7b6340fd3703848f3e2441d1f
Your Name <mcpgco@gmail.com>
2022-03-07 22:27:17 -0700

    started from scratch lost in translation to get each block working focusing on getting 1 block working

--- java.js
+++ java.js
@@ -2,3 +2,7 @@ $(document).ready(function () {// essentially tells engine to load 1)html & 2)cs
     //display current day & time.
     $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
 });
+
+
+
+$("#hour9 .description").val(localStorage.getItem("hour9"));
